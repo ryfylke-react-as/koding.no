fe298ba90bc07c0948ad350fe506d9867784cf6c
Håkon <haakonu97@gmail.com>
2023-07-21 13:26:55 +0200

    chore: mono font, social card fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpeg",
+      image: "img/social-card.png",
       navbar: {
         hideOnScroll: true,
         title: "Koding.no",
