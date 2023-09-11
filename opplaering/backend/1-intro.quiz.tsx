c625c812878018995c79eccdeee00b8b4c045da5
Marie Katrine Ekeberg <mke@themkat.net>
2023-09-11 17:18:45 +0200

    Endre svar i backend quiz til å starte med stor bokstav

--- opplaering/backend/1-intro.quiz.tsx
+++ opplaering/backend/1-intro.quiz.tsx
@@ -10,9 +10,9 @@ const data: KompetanseTestData = {
     {
       sporsmal: "Hvilke av følgende er IKKE et ansvarsområde for backend?",
       svar: [
-        "å validere mottatt data",
-        "sørge for at frontend har riktig farger",
-        "lagre data i database for senere bruk",
+        "Å validere mottatt data",
+        "Sørge for at frontend har riktig farger",
+        "Lagre data i database for senere bruk",
       ],
       korrekt: 1,
     },
@@ -20,17 +20,17 @@ const data: KompetanseTestData = {
       sporsmal: "Hvilken av følgende er en backend utvikler sin oppgave?",
       svar: [
         "Designe brukergrensesnitt",
-        "å sørge for at web-applikasjon kjører i riktig skjermstørrelse",
-        "utvikle applikasjonslogikk som behandler data på en server",
+        "Å sørge for at web-applikasjon kjører i riktig skjermstørrelse",
+        "Utvikle applikasjonslogikk som behandler data på en server",
       ],
       korrekt: 2,
     },
     {
       sporsmal: "Hva er FEIL om DevOps under?",
       svar: [
-        "å ha overlevering av programvaren fra utvikling til drift, så drift kan deploye den",
-        "å automatisere mest mulig for å øke produktivitet",
-        "å ha fokus på kontinuerlig leveranser, som i noen tilfeller kan være så ofte som hver time",
+        "Å ha overlevering av programvaren fra utvikling til drift, så drift kan deploye den",
+        "Å automatisere mest mulig for å øke produktivitet",
+        "Å ha fokus på kontinuerlig leveranser, som i noen tilfeller kan være så ofte som hver time",
       ],
       korrekt: 0,
     },
